82b531d15fc5427a97f3e649b2f8b9df546cfb0a
Andyngo280 <Andyngo28@gmail.com>
2023-05-05 01:07:56 -0700

    Updated Images and added Images for endDialog

--- endDialog.h
+++ endDialog.h
@@ -17,24 +17,29 @@ public:
 		//wxPanel scorePanel = new wxPanel(this);
 		//wxPanel numPanel = new wxPanel(this);
 
-		wxString total = wxString::Format(wxT("Your Score: %i | Total Score: %i\n"), score, max);
-		wxStaticText* label1 = new wxStaticText(this, wxID_ANY, total); //wxDefaultPosition, wxDefaultSize, wxST_ELLIPSIZE_START
-		label1->SetMinSize(wxSize(300, -1));
-		//label1->Wrap(1000);
-		scoreSizer->Add(label1);
-
-
 		int percentage = score * 10; 
 
+		wxString total = wxString::Format(wxT("Your Score: %i | Total Score: %i | Percentage: %i% \n"), score, max, percentage);
+		wxStaticText* label1 = new wxStaticText(this, wxID_ANY, total); //wxDefaultPosition, wxDefaultSize, wxST_ELLIPSIZE_START
+		label1->SetMinSize(wxSize(350, -1));
+		//label1->Wrap(1000);
+		scoreSizer->Add(label1);
+		
+		//Display Title for the score
 		wxString finalTitle = displayFinalTitle(percentage);
 		wxStaticText* label2 = new wxStaticText(this, wxID_ANY, finalTitle); // wxDefaultPosition, wxDefaultSize, wxST_ELLIPSIZE_START)
 		label2->SetMinSize(wxSize(300, -1));
 		//label2->Wrap(1000);
 		scoreSizer->Add(label2);
-		
-		/*wxStaticText* label2 = new wxStaticText(this, wxID_ANY, _("You got:"));
-		label2->SetMinSize(wxSize(200, -1));
-		scoreSizer->Add(label2)*/
+
+		mainSizer->AddSpacer(10);
+
+		//display image for the score
+		wxString fileName = getFileName(percentage);
+		wxImage image(fileName, wxBITMAP_TYPE_PNG);
+		wxBitmap bitmap(image); //load image file 
+		wxStaticBitmap* staticBitMap = new wxStaticBitmap(this, wxID_ANY, bitmap, wxDefaultPosition, wxDefaultSize); //create bitmap to display the image 
+		scoreSizer->Add(staticBitMap, 0, wxALIGN_CENTER_HORIZONTAL|wxALL, 10); //add the bitmap controller to the sizer. 
 		
 		mainSizer->Add(scoreSizer);
 		
@@ -48,6 +53,7 @@ private:
 	wxString displayFinalTitle(int val)
 	{
 		wxString titleScore; 
+
 		if (val == 100){titleScore = "A+ for Arceus";}
 		else if (val >= 90 && val <= 99) {titleScore =  "A upcoming Pokemon Monster";}	
 		else if (val >= 80 && val <= 89) {titleScore =  "B blasting off again";}
@@ -58,4 +64,20 @@ private:
 
 		return titleScore; 
 	}
+
+	wxString getFileName(int val)
+	{
+		wxString picture; 
+
+		if (val == 100){picture = "images/Arceus.png";}
+		else if (val >= 90 && val <= 99) {picture=  "images/Ash_Master.png";}	
+		else if (val >= 80 && val <= 89) {picture = "images/TeamRocketBlastingOff.png";}
+		else if (val >= 70 && val <= 79) {picture = "images/Chansey.png";}
+		else if (val >= 60 && val <= 69) {picture = "images/diglett-and-dugtrio-pack.png";}
+		else if (val >= 1 && val <= 59) {picture = "images/farfetch.png";}
+		else picture = "images/Magikarp.png";
+
+		return picture;
+	}
+
 };
